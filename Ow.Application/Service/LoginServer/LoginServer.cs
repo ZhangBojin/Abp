c82c6d8f5c05f1633fa84a98c599f45a6c3c51d9
BorgenZ <bojin.zhang@linexsolutions.com>
2024-08-27 18:15:33 +0800

    --

---
+++ Ow.Application/Service/LoginServer/LoginServer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Identity;
+
+namespace Ow.Application.Service.LoginServer
+{
+    public class LoginServer(IdentityUserManager identityUserManager,IdentityRoleManager identityRoleManager) : ApplicationService
+    {
+        
+        [AllowAnonymous]
+        public void Login()
+        {
+        }
+    }
+}
---
+++ WebApplication1/Helper/JwtHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApplication1.Helper
+{
+    public static class JwtHelper
+    {
+        public static string GenerateJwtToken(string user)
+        {
+            // 设置JWT的密钥
+            const string secretKey = "{19B2CAF7-095D-44E9-A45D-C8A8F5C16414}";
+            var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+            var securityKey = new SymmetricSecurityKey(keyBytes);
+
+            // 创建JWT的签名凭证
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // 设置JWT的Claims
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user),
+                // 添加其他需要的声明
+            };
+
+            // 创建JWT的Token
+            var token = new JwtSecurityToken(
+                issuer: "zbj",
+                audience: "ZBJ",
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: signingCredentials
+            );
+
+            // 生成JWT字符串
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwtToken;
+        }
+    }
+}
